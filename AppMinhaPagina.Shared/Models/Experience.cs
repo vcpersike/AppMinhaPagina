09141d6f7c9e6cbf8363e552cb99e75db7020c34
vcpersike <vcpersike@gmail.com>
2025-02-09 03:24:57 -0300

    Novas telas de teste

---
+++ AppMinhaPagina.Shared/Models/Experience.cs
@@ -0,0 +1,10 @@
+﻿namespace AppMinhaPagina.Shared.Models
+{
+    public class Experience
+    {
+        public string Empresa { get; set; }
+        public string Cargo { get; set; }
+        public string Periodo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
