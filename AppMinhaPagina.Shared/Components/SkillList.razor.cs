029c5088508566125ebb7a0623d5935a4a72fa02
vcpersike <vcpersike@gmail.com>
2025-02-09 03:55:48 -0300

    Ajustes leves sobre estilos

---
+++ AppMinhaPagina.Shared/Components/SkillList.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AppMinhaPagina.Shared.Components
+{
+    public partial class SkillList : ComponentBase
+    {
+        public List<string> Skills { get; set; } = new()
+        {
+            "C#", "Angular", "React", "Docker", "Azure DevOps", "DDD", "SOLID", "GO"
+        };
+    }
+}
---
+++ AppMinhaPagina.Shared/Pages/Home.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using AppMinhaPagina.Shared.Models;
+
+namespace AppMinhaPagina.Shared.Pages
+{
+    public partial class Home : ComponentBase
+    {
+        public List<Experience> MyExperiences { get; set; } = new()
+        {
+            new Experience
+            {
+                Empresa = "Ingram Micro",
+                Cargo = "Desenvolvedor Full-Stack",
+                Periodo = "10/23 - Atual",
+                Descricao = "Será Atuo na squad de contabilidade, desenvolvendo soluções em C#, React e Azure."
+            },
+            new Experience
+            {
+                Empresa = "Totvs Meu Protheus",
+                Cargo = "Desenvolvedor Mobile",
+                Periodo = "07/23 - 02/24",
+                Descricao = "Desenvolvimento de apps móveis com Angular 15 e Ionic, integrando Firebase."
+            }
+        };
+    }
+}
