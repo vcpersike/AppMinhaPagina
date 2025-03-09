8586e6ef33325887ac631567209af1f46fb2c8b1
vcpersike <vcpersike@gmail.com>
2025-03-09 03:55:49 -0300

    Ajuste de habilidades e inclusão da foto

--- AppMinhaPagina.Shared/Services/ContactService.cs
+++ AppMinhaPagina.Shared/Services/ContactService.cs
@@ -1,21 +1,15 @@
-﻿namespace AppMinhaPagina.Shared.Services;
-
-using AppMinhaPagina.Shared.Models;
+﻿using AppMinhaPagina.Shared.Models;
 using AppMinhaPagina.Shared.Services.Interface;
-using System.Threading.Tasks;
 
 public class ContactService : IContactService
 {
     public async Task<ContactModel> GetContactAsync()
     {
-        // Simulando um carregamento assíncrono
-        await Task.Delay(100);
-
         return new ContactModel
         {
             Email = "dev.victor.persike@gmail.com",
             Phone = "+55 11 95705-7466",
-            Website = "https://www.victorpersike.dev.br"
+            Website = "https://victorpersike.dev.br"
         };
     }
 }
